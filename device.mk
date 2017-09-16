c720d47b70c4af678d18910bd2845601dee79050
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-09-16 17:38:49 +0300

    Add my overlay with translations

--- device.mk
+++ device.mk
@@ -19,7 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    vendor/hnt/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
